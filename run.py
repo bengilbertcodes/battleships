0fd267104b762b33660348976f2ffa7ea4e72c09
Ben <144111469+bengilbertcodes@users.noreply.github.com>
2024-01-11 15:27:08 +0000

    import random module

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 from colorama import Fore, Back, Style
-
+import random
 
 print('')
 print('XXXX     X   XXXXX XXXXX X     XXXXX  XXXX X   X XXXXX XXXX  XXXX')
@@ -9,10 +9,10 @@ print('X   X XXXXXXX  X     X   X     X         X X   X   X   X        X')
 print('XXXX  X     X  X     X   XXXXX XXXXX  XXXX X   X XXXXX X     XXXX')
 print('')
 
-CPU_BOARD = [[' '] * 8 for x in range(8)]
+ENEMY_BOARD = [[' '] * 8 for x in range(8)]
 PLAYER_BOARD = [[' '] * 8 for x in range(8)]
 
-def print_board(board):
+def create_board(board):
     print('')
     print('  A B C D E F G H')
     print(' -----------------')
@@ -20,6 +20,8 @@ def print_board(board):
     for row in board:
         print("%d|%s|" % (row_number, '|'.join(row)))
         row_number += 1
-        
-print_board(CPU_BOARD)
-print_board(PLAYER_BOARD)
+
+
+create_board(ENEMY_BOARD)
+create_board(PLAYER_BOARD)
+
